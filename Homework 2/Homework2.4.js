a167d18e5b5886a31410f8b73552f994609a9418
Elenachg <chaikovskaya.olena@gmail.com>
2021-02-16 21:15:15 +0200

    New Homework

--- Homework 2/Homework2.4.js
+++ Homework 2/Homework2.4.js
@@ -2,11 +2,12 @@
 Найти сумму цифр числа, которые кратны двум
 */
 
+let num = 1234567;
 let sum = 0;
 
-for(let i = 0; i < 80; i++){
-	if (i % 2 === 0){
-		sum += i;
+for(let i of num.toString()){
+	if (+i % 2 === 0){
+		sum += +i;
 	}
 }
 console.log(sum);
